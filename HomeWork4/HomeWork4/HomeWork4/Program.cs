3a4997dabee15ff19c11634734f2f1af968ab498
Nerocus <garvitev@gmail.com>
2021-10-27 18:20:57 +0300

    Home4.main

--- HomeWork4/HomeWork4/HomeWork4/Program.cs
+++ HomeWork4/HomeWork4/HomeWork4/Program.cs
@@ -9,60 +9,68 @@ namespace HomeWork4
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             int[,] num1 = {
                 { 1, 100000, 80000, 20000 },
                 { 2, 120000, 90000, 30000 },
-                { 3,80000,70000,    10000 },
-                { 4,70000 , 70000 , 0},
+                { 3,80000,70000,    1000 },
+                { 4,70000 , 70000 , 100000},
                 { 5 ,100000,80000 ,20000 },
                 { 6,200000 ,120000,80000 },
-                { 7, 130000 ,140000, -10000 },
+                { 7, 130000 ,140000, 10000 },
                 { 8,150000 ,65000,85000 },
                 { 9 ,190000, 90000, 100000 },
                 { 10 , 110000 , 70000 ,  40000},
                 { 11 , 150000 , 120000 ,9000 },
                 { 12,100000,80000,20000 }
             };
-            int minValue1 = num1[0, 3];
-            int minValue2 = num1[0, 3];
-            int minValue3 = num1[0, 3];
+            int minProfit = 20000;
+            int minValue = 0;
+            int Month = 0;
 
-            int minMonth1 = num1[0, 0];
-            int minMonth2 = num1[0, 0];
-            int minMonth3 = num1[0, 0];
             for (int i = 0; i < num1.GetLength(0); i++)
             {
-                if (num1[i, 3] < minValue1)
+                if (num1[i, 3] <= minProfit)
                 {
-                    minValue3 = minValue2;
-                    minMonth3 = num1[i, 0];
-
-                    minValue2 = minValue1;
-                    minMonth2 = num1[i, 0];
-
-                    minValue1 = num1[i, 3];
-                    minMonth1 = num1[i, 0];
-                    
-                    
-                }
-                else if (minValue2 > num1[i, 3])
-                {
-                    minValue3 = minValue2;
-                    minMonth3 = num1[i, 0];
-
-                    minValue2 = num1[i, 3];
-                    minMonth2 = num1[i, 0];
-                }
-                else if (minValue3 > num1[i, 3])
-                {
-                    minValue3 = num1[i, 3];
-                    minMonth3 = num1[i, 0];
+                    Month = 
+                    minValue = num1[i, 3];
+                    Console.WriteLine($"{Month},{minValue}");
                 }
             }
-            Console.WriteLine($"{minMonth1},{minValue1}");
-            Console.WriteLine($"{minMonth2},{minValue2}");
-            Console.WriteLine($"{minMonth3},{minValue3}");
+            Console.ReadKey();*/
+
+
+            Console.Write("Введите количество строк : ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            int[][] triangle = new int[n][];
+            // первая строка
+            triangle[0] = new int[] { 1 };
+
+            for (int i = 1; i < triangle.Length; i++)
+            {
+                triangle[i] = new int[i + 1];
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || j == i)
+                        triangle[i][j] = 1;
+                    else
+                    {
+                        triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < triangle.Length; i++)
+            {
+                for (int j = 0; j < triangle[i].Length; j++)
+                {
+                    Console.Write("{0,-3} ", triangle[i][j]);
+                }
+                Console.WriteLine();
+            }
+
             Console.ReadKey();
         }
     }
